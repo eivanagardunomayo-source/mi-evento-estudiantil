1b25116d4c4e07ea5311d806ca56decad94c91a1
Mariela Gutierrez <marielagutierrez@MacBook-Air-de-Mariela.local>
2026-05-14 14:25:44 -0600

    chore: eliminar debug-token, volver a 12 funciones

--- api/debug-token.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function handler(req, res) {
-  const token = process.env.NOTION_TOKEN || '';
-  res.status(200).json({
-    tiene_token: !!token,
-    longitud: token.length,
-    primeros_10: token.substring(0, 10),
-    db_asistencia: (process.env.NOTION_ASISTENCIA_DB_ID || '').substring(0, 10),
-  });
-};
